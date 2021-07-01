77cf01197e8c550fd589cbfad69d72267618b049
Gabriel Macias <4369637+Soarxyn@users.noreply.github.com>
2021-07-01 17:05:01 -0300

    Program is finished.

--- EP1/main.py
+++ EP1/main.py
@@ -545,6 +545,24 @@ def plot_1():
 
     plt.show()
 
+def plot_spring(x0, x, ax):
+    """
+        Desenha uma mola.
+        Este código foi baseado na implementação de Christian, do blog Scipython. 
+        Disponível em: <https://scipython.com/blog/the-spring-pendulum/>. Acesso: 01 Jul. 2021.
+    """
+    L = np.abs(x-x0)
+    rs, ns = 0.03, 5
+    Ns = 1000
+    ipad1, ipad2 = 100, 100
+    w = np.linspace(0, L, Ns)
+    xp = np.zeros(Ns)
+    xp[ipad1:-ipad2] = rs * np.sin(2*np.pi * ns * w[ipad1:-ipad2] / L)
+    R = np.array([[np.cos(pi/2), -np.sin(pi/2)],
+                  [np.sin(pi/2), np.cos(pi/2)]])
+    xs, ys = - R @ np.vstack((xp, w))
+    return ax.plot(x0 * np.ones(len(xs)) + xs, ys, c='silver', lw = 2)
+
 def plot_2():
     """
         Exibe os gráficos e animações associados ao problema 2.
@@ -612,14 +630,22 @@ def plot_2():
             mat4 = axes[1, 1].plot([], [], color = "coral", lw = 1.5)
             axes[1, 2].set_xlim(-10, xlim)
             mat5 = axes[1, 2].plot([], [], 'o')
+            
+            mola1 = plot_spring(0, 1, axes[1,2])
+            mola2 = plot_spring(0, 1, axes[1,2])
+            mola3 = plot_spring(0, 1, axes[1,2])
+            mola4 = plot_spring(0, 1, axes[1,2])
+            molapar1 = plot_spring(0, 1, axes[1,2])
+            molapar2 = plot_spring(0, 1, axes[1,2])
 
-            patches = list(mat0) + list(mat1) + list(mat2) + list(mat3) + list(mat4) + list(mat5)
+            patches = list(mat0) + list(mat1) + list(mat2) + list(mat3) + list(mat4) + list(mola1) + list(mola2) + list(mola3) + list(mola4) + list(molapar1) + list(molapar2) + list(mat5)
 
             axes[0, 0].set_ylim(-ylim, ylim)
             axes[0, 1].set_ylim(-ylim, ylim)
             axes[0, 2].set_ylim(-ylim, ylim)
             axes[1, 0].set_ylim(-ylim, ylim)
             axes[1, 1].set_ylim(-ylim, ylim)
+            axes[1, 2].set_ylim(-1, 1)
 
             def init():
                 global time
@@ -656,6 +682,21 @@ def plot_2():
                 Y = np.zeros(np.shape(X))
 
                 mat5[0].set_data(X,Y)
+
+                a = plot_spring(X[0], X[1], axes[1, 2])[0]
+                b = plot_spring(X[1], X[2], axes[1, 2])[0]
+                c = plot_spring(X[2], X[3], axes[1, 2])[0]
+                d = plot_spring(X[3], X[4], axes[1, 2])[0]
+                e = plot_spring(-10, X[0], axes[1, 2])[0]
+                f = plot_spring(X[4], xlim, axes[1, 2])[0]
+
+                mola1[0].set_data(a.get_xdata(), a.get_ydata())
+                mola2[0].set_data(b.get_xdata(), b.get_ydata())
+                mola3[0].set_data(c.get_xdata(), c.get_ydata())
+                mola4[0].set_data(d.get_xdata(), d.get_ydata())
+                molapar1[0].set_data(e.get_xdata(), e.get_ydata())
+                molapar2[0].set_data(f.get_xdata(), f.get_ydata())
+
                 return patches
             
             for i, axis in enumerate(axes.flat[:-1]):
@@ -678,45 +719,58 @@ def plot_3():
         Exibe os gráficos e animações associados ao problema 3.
     """
     print("")
-    k = [40 + 2 * i for i in range(1, 7)]
+    k = [40 + 2 * (-1) ** i for i in range(1, 12)]
 
     alphas = np.array([(a + b)/2 for (a, b) in zip(k, k[1:])])
     betas = np.array([-b/2 for b in k[1:-1]])
 
     (alphas_k, _, V, _) = qr_algorithm(alphas, betas)
 
-    initial_conditions = [np.array([-2.0, -3.0, -1.0, -3.0, -1.0]), np.array([1.0, 10.0, -4.0, 3.0, -2.0]), V[:, 0]]
-    mass_positions = [[10 * i for i in range(0, 6)], [20 * i for i in range(0, 6)], [10 * i for i in range(0, 6)]]
-    xlims = [50, 90, 50]
-    ylims = [4, 12, 2]
+    initial_conditions = [np.array([-2.0, -3.0, -1.0, -3.0, -1.0, -2.0, -3.0, -1.0, -3.0, -1.0]), np.array([1.0, 10.0, -4.0, 3.0, -2.0, 1.0, 10.0, -4.0, 3.0, -2.0]), V[:, 0]]
+    mass_positions = [[10 * i for i in range(0, 11)], [20 * i for i in range(0, 11)], [10 * i for i in range(0, 11)]]
+    xlims = [100, 190, 100]
 
-    for (X0, P0, xlim, ylim) in zip(initial_conditions, mass_positions, xlims, ylims):
+    for (X0, P0, xlim) in zip(initial_conditions, mass_positions, xlims):
         print(f"      Exibindo gráfico para X(0) = {X0}")
         Y0 = np.matmul(np.transpose(V), X0)
         
         time = np.linspace(0, 10, int(10 / 0.025))
-        solution = np.array([[sum([V[i][j] * Y0[j] * np.cos(np.sqrt(alphas_k[j]) * t) for j in range(5)]) for t in time] for i in range(5)])
+        solution = np.array([[sum([V[i][j] * Y0[j] * np.cos(np.sqrt(alphas_k[j]) * t) for j in range(10)]) for t in time] for i in range(10)])
 
         fig, axes = plt.subplots(2, 3)
+        plt.figtext(0.5, .94, 'Deslocamento da Mola em relação ao Equilíbrio', fontsize = 10, ha = "center")
         fig.set_size_inches(18.5, 10.5)
         fig.suptitle('Evolução do Sistema no Tempo')
         fig.subplots_adjust(hspace = 0.4)
-        fig.delaxes(axes[1, 2])
-
-        plt.figtext(0.5, .94, 'Deslocamento da Mola em relação ao Equilíbrio', fontsize = 10, ha = "center")
-
         axes[0, 0].plot(time, solution[0], color = "blue", lw = 1.5)
         axes[0, 1].plot(time, solution[1], color = "red", lw = 1.5)
         axes[0, 2].plot(time, solution[2], color = "lime", lw = 1.5)
         axes[1, 0].plot(time, solution[3], color = "purple", lw = 1.5)
         axes[1, 1].plot(time, solution[4], color = "coral", lw = 1.5)
+        axes[1, 2].plot(time, solution[5], color = "orange", lw = 1.5)
 
         for i, axis in enumerate(axes.flat):
             axis.set_ylabel("Deslocamento (cm)")
             axis.set_xlabel("Tempo (s)")
             axis.set_title(f"Massa {i+1}")
 
-        plt.show()  
+        fig, axes = plt.subplots(2, 3)
+        fig.set_size_inches(18.5, 10.5)
+        fig.subplots_adjust(hspace = 0.4)
+        fig.delaxes(axes[1, 1])
+        fig.delaxes(axes[1, 2])
+
+        axes[0, 0].plot(time, solution[6], color = "magenta", lw = 1.5)
+        axes[0, 1].plot(time, solution[7], color = "pink", lw = 1.5)
+        axes[0, 2].plot(time, solution[8], color = "lavender", lw = 1.5)
+        axes[1, 0].plot(time, solution[9], color = "gold", lw = 1.5)
+
+        for i, axis in enumerate(axes.flat[:-2]):
+            axis.set_ylabel("Deslocamento (cm)")
+            axis.set_xlabel("Tempo (s)")
+            axis.set_title(f"Massa {i+7}")
+
+        plt.show()
 
         option = input("      Deseja visualizar uma animação para este caso? (S/n): ")
 
@@ -724,80 +778,81 @@ def plot_3():
             print("")
 
             time = []
-        
-            solution = [[], [], [], [], []]
+            solution = [[], [], [], [], [], [], [], [], [], []]
 
-            fig, axes = plt.subplots(2, 3)
-            plt.figtext(0.5, .94, 'Deslocamento da Mola em relação ao Equilíbrio', fontsize = 10, ha = "center")
+            fig = plt.figure()
             fig.set_size_inches(18.5, 10.5)
-            fig.suptitle('Evolução do Sistema no Tempo')
-            fig.subplots_adjust(hspace = 0.4)
+            fig.suptitle("Evolução do Sistema no Tempo")
 
-            mat0 = axes[0, 0].plot([], [], color = "blue", lw = 1.5)
-            mat1 = axes[0, 1].plot([], [], color = "red", lw = 1.5)
-            mat2 = axes[0, 2].plot([], [], color = "lime", lw = 1.5)
-            mat3 = axes[1, 0].plot([], [], color = "purple", lw = 1.5)
-            mat4 = axes[1, 1].plot([], [], color = "coral", lw = 1.5)
-            axes[1, 2].set_xlim(-10, xlim)
-            mat5 = axes[1, 2].plot([], [], 'o')
+            ax = fig.add_subplot(111)
+            ax.set_yticks([])
+            ax.set_xlim(-15, xlim)
+            ax.set_ylim(-1, 1)
 
-            patches = list(mat0) + list(mat1) + list(mat2) + list(mat3) + list(mat4) + list(mat5)
+            mat = ax.plot([], [], 'o')
+                 
+            mola1 = plot_spring(0, 1, ax)
+            mola2 = plot_spring(0, 1, ax)
+            mola3 = plot_spring(0, 1, ax)
+            mola4 = plot_spring(0, 1, ax)
+            mola5 = plot_spring(0, 1, ax)
+            mola6 = plot_spring(0, 1, ax)
+            mola7 = plot_spring(0, 1, ax)
+            mola8 = plot_spring(0, 1, ax)
+            mola9 = plot_spring(0, 1, ax)
+            molapar1 = plot_spring(0, 1, ax)
+            molapar2 = plot_spring(0, 1, ax)
 
-            axes[0, 0].set_ylim(-ylim, ylim)
-            axes[0, 1].set_ylim(-ylim, ylim)
-            axes[0, 2].set_ylim(-ylim, ylim)
-            axes[1, 0].set_ylim(-ylim, ylim)
-            axes[1, 1].set_ylim(-ylim, ylim)
+            patches = list(mola1) + list(mola2) + list(mola3) + list(mola4) + list(mola5) + list(mola6) + list(mola7) + list(mola8) + list(mola9) + list(molapar1) + list(molapar2) + list(mat)
 
             def init():
                 global time
                 global solution
                 time = []
-                solution = [[], [], [], [], []]
-                mat0[0].set_data([], [])
-                mat1[0].set_data([], [])
-                mat2[0].set_data([], [])
-                mat3[0].set_data([], [])
-                mat4[0].set_data([], [])
-                mat5[0].set_data([], [])
+                solution = [[], [], [], [], [], [], [], [], [], []]
+                mat[0].set_data([], [])
+
                 return patches
 
             def animate(index):
                 t = index * 0.025
                 time.append(t)
 
-                for i in range(5):
-                    solution[i].append(sum([V[i][j] * Y0[j] * np.cos(np.sqrt(alphas_k[j]) * t) for j in range(5)]))
+                for i in range(10):
+                    solution[i].append(sum([V[i][j] * Y0[j] * np.cos(np.sqrt(alphas_k[j]) * t) for j in range(10)]))
 
-                mat0[0].set_data(time, solution[0])
-                mat1[0].set_data(time, solution[1])
-                mat2[0].set_data(time, solution[2])
-                mat3[0].set_data(time, solution[3])
-                mat4[0].set_data(time, solution[4])
-                mat0[0].axes.set_xlim(t - 1, t)
-                axes[0, 1].set_xlim(t - 1, t)
-                axes[0, 2].set_xlim(t - 1, t)
-                axes[1, 0].set_xlim(t - 1, t)
-                axes[1, 1].set_xlim(t - 1, t)
-
-                X = np.array([P0[i] + solution[i][index] for i in range(5)])
+                X = np.array([P0[i] + solution[i][index] for i in range(10)])
                 Y = np.zeros(np.shape(X))
 
-                mat5[0].set_data(X,Y)
+                mat[0].set_data(X,Y)
+
+                a = plot_spring(X[0], X[1], ax)[0]
+                b = plot_spring(X[1], X[2], ax)[0]
+                c = plot_spring(X[2], X[3], ax)[0]
+                d = plot_spring(X[3], X[4], ax)[0]
+                e = plot_spring(X[4], X[5], ax)[0]
+                f = plot_spring(X[5], X[6], ax)[0]
+                g = plot_spring(X[6], X[7], ax)[0]
+                h = plot_spring(X[7], X[8], ax)[0]
+                i = plot_spring(X[8], X[9], ax)[0]
+                j = plot_spring(-15, X[0], ax)[0]
+                k = plot_spring(X[9], xlim, ax)[0]
+
+                mola1[0].set_data(a.get_xdata(), a.get_ydata())
+                mola2[0].set_data(b.get_xdata(), b.get_ydata())
+                mola3[0].set_data(c.get_xdata(), c.get_ydata())
+                mola4[0].set_data(d.get_xdata(), d.get_ydata())
+                mola5[0].set_data(e.get_xdata(), e.get_ydata())
+                mola6[0].set_data(f.get_xdata(), f.get_ydata())
+                mola7[0].set_data(g.get_xdata(), g.get_ydata())
+                mola8[0].set_data(h.get_xdata(), h.get_ydata())
+                mola9[0].set_data(i.get_xdata(), i.get_ydata())
+                molapar1[0].set_data(j.get_xdata(), j.get_ydata())
+                molapar2[0].set_data(k.get_xdata(), k.get_ydata())
+
                 return patches
-            
-            for i, axis in enumerate(axes.flat[:-1]):
-                axis.set_ylabel("Deslocamento (cm)")
-                axis.set_title(f"Massa {i+1}")
-                axis.set_xticks([])
 
             anim = FuncAnimation(fig, animate, init_func=init, frames=1000000, interval=10, blit=True)
-
-            for i, axis in enumerate(axes.flat[:-1]):
-                axis.set_ylabel("Deslocamento (cm)")
-            axes[1, 2].set_xlabel("Posição (cm)")
-            axes[1, 2].set_yticks([])
-            axes[1, 2].set_title("Sistema")
 
             plt.show()
 
