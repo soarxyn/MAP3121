8e28f3fd18ffc87e28dfed0742b919de91b97c81
Aimnos <roikegami@hotmail.com>
2021-07-20 16:50:21 -0300

    Minor code refactoring

--- EP2/main.py
+++ EP2/main.py
@@ -254,10 +254,6 @@ def qr_algorithm(
     return (alphas_k, betas_k, V, iterations)
 
 
-# def Householder(x: np.array, w: np.array) -> np.array:
-#     return x - 2 * np.dot(w, x) / np.dot(w, w) * w
-
-
 def tridiagonalization(A: np.array) -> Tuple[np.array, np.array, np.array]:
     A = A.copy()
     alphas = []
@@ -269,26 +265,23 @@ def tridiagonalization(A: np.array) -> Tuple[np.array, np.array, np.array]:
         w_i = A[1:, 0]
 
         alphas.append(A[0, 0])
-        betas.append(np.sqrt(np.dot(w_i, w_i)))
+        betas.append(-sgn(w_i[0]) * np.sqrt(np.dot(w_i, w_i)))
 
         w_i[0] -= betas[-1]
         w_i2 = np.dot(w_i, w_i)
 
         A = A[1:, 1:]
 
-        for col in np.transpose(A):
-            col -= 2 * np.dot(w_i, col) / w_i2 * w_i
-
-        for row in A:
-            row -= 2 * np.dot(w_i, row) / w_i2 * w_i
-
-        for row in H[:, -m:]:
-            row -= 2 * np.dot(w_i, row) / w_i2 * w_i
+        for Acol, Arow, Hrow in zip(np.transpose(A), A, H[:, -m:]):
+            Acol -= 2 * np.dot(w_i, Acol) / w_i2 * w_i
+            Arow -= 2 * np.dot(w_i, Arow) / w_i2 * w_i
+            Hrow -= 2 * np.dot(w_i, Hrow) / w_i2 * w_i
 
     alphas.extend(np.diag(A))
     betas.append(A[1, 0])
 
     return (np.array(alphas), np.array(betas), H)
+
 
 def lines_to_rows(lines):
     line_to_list = map(lambda l: l.split(), lines)
@@ -296,40 +289,54 @@ def lines_to_rows(lines):
 
     return enumerate(str_to_float)
 
+
 def matrix_from_file(filename):
-    with open(filename, encoding = 'utf-8') as file:
-        matrix_size : int = int(file.readline())
+    with open(filename, encoding="utf-8") as file:
+        matrix_size: int = int(file.readline())
         matrix = np.zeros((matrix_size, matrix_size))
 
         for i, line in lines_to_rows(file.readlines()):
-            matrix[i,:] = line
+            matrix[i, :] = line
 
     return matrix
 
+
 def teste_1():
-    print("""
+    print(
+        """
       >> Você selecionou o teste A proposto pelo relatório.
       
-      - Realizando leitura de arquivo: 'input-a'\n""")
+      - Realizando leitura de arquivo: 'input-a'\n"""
+    )
 
-    A = matrix_from_file('input-a')
+    A = matrix_from_file("input-a")
 
     print("""      Matriz de entrada:\n""")
-    print("     ", np.array2string(A, prefix = "      "))
+    print("     ", np.array2string(A, prefix="      "))
 
     alphas, betas, H = tridiagonalization(A)
 
     print("""\n      Matriz tridiagonalizada:\n""")
-    print("     ", np.array2string(np.diag(betas, k = 1) + np.diag(betas, k = -1) + np.diag(alphas), prefix = "      "))
+    print(
+        "     ",
+        np.array2string(
+            np.diag(betas, k=1) + np.diag(betas, k=-1) + np.diag(alphas),
+            prefix="      ",
+        ),
+    )
 
     Lambda, _, V, _ = qr_algorithm(alphas, betas, H)
 
-    print(f"\n      Autovalores:\n\t- Encontrados:\t{np.array(sorted(Lambda, reverse = True))}\n\t- Esperados:\t{np.array([7.0, 2.0, -1.0, -2.0])}")
+    print(
+        f"\n      Autovalores:\n\t- Encontrados:\t{np.array(sorted(Lambda, reverse = True))}\n\t- Esperados:\t{np.array([7.0, 2.0, -1.0, -2.0])}"
+    )
     print(f"\n      Matriz de Autovetores:\n")
     print("\t    v1\t      v2\tv3\t   v4")
-    print("     ", np.array2string(V, prefix = "      "))
+    print("     ", np.array2string(V, prefix="      "))
 
-    print("\n      OBS.: Caso λ não seja renderizado corretamente em seu terminal, este \n      char é um Lambda, indicando o autovalor da matriz A.")
+    print(
+        "\n      OBS.: Caso λ não seja renderizado corretamente em seu terminal, este \n      char é um Lambda, indicando o autovalor da matriz A."
+    )
 
     for i in range(len(Lambda)):
         result = np.matmul(A, V[:, i])
@@ -347,43 +354,63 @@ def teste_1():
         )
 
         input("\n     Pressione [ENTER] para continuar.")
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[2K')
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[2K')
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
         print("      -")
 
     print(f"\n      Teste de ortogonalidade:\n")
-    print("      VVt =", np.array2string(np.matmul(V, np.transpose(V)), prefix = "            "))
+    print(
+        "      VVt =",
+        np.array2string(np.matmul(V, np.transpose(V)), prefix="            "),
+    )
 
     print("\n      Rotina de teste concluída! Obrigado pela execução!")
 
+
 def teste_2():
-    print("""
+    print(
+        """
       >> Você selecionou o teste B proposto pelo relatório.
       
-      - Realizando leitura de arquivo: 'input-b'\n""")
+      - Realizando leitura de arquivo: 'input-b'\n"""
+    )
 
-    A = matrix_from_file('input-b')
+    A = matrix_from_file("input-b")
 
     print("""      Matriz de entrada:\n""")
-    print("     ", np.array2string(A, prefix = "      "))
+    print("     ", np.array2string(A, prefix="      "))
 
     alphas, betas, H = tridiagonalization(A)
 
     n, _ = A.shape
-    expectedEigenvalues = np.array( [1.0 / (2 * (1 - np.cos((2 * i + 1) * pi / (2 * n + 1)))) for i in range(n)] )
+    expectedEigenvalues = np.array(
+        [1.0 / (2 * (1 - np.cos((2 * i + 1) * pi / (2 * n + 1)))) for i in range(n)]
+    )
     print("""\n      Matriz tridiagonalizada:\n""")
-    print("     ", np.array2string(np.diag(betas, k = 1) + np.diag(betas, k = -1) + np.diag(alphas), prefix = "      "))
+    print(
+        "     ",
+        np.array2string(
+            np.diag(betas, k=1) + np.diag(betas, k=-1) + np.diag(alphas),
+            prefix="      ",
+        ),
+    )
 
     Lambda, _, V, _ = qr_algorithm(alphas, betas, H)
 
-    print(f"\n      Autovalores:\n\t- Encontrados:\t{np.array(sorted(Lambda, reverse = True))}\n\t- Esperados:\t{expectedEigenvalues}")
+    print(
+        f"\n      Autovalores:\n\t- Encontrados:\t{np.array(sorted(Lambda, reverse = True))}\n\t- Esperados:\t{expectedEigenvalues}"
+    )
     print(f"\n      Matriz de Autovetores:\n")
-    print("\t    v1\t      v2\tv3\t   v4\t     v5\t  ...\t  v16\t    v17\t      v18\tv19\t  v20")
-    print("     ", np.array2string(V, prefix = "      "))
+    print(
+        "\t    v1\t      v2\tv3\t   v4\t     v5\t  ...\t  v16\t    v17\t      v18\tv19\t  v20"
+    )
+    print("     ", np.array2string(V, prefix="      "))
 
-    print("\n      OBS.: Caso λ não seja renderizado corretamente em seu terminal, este \n      char é um Lambda, indicando o autovalor da matriz A.")
+    print(
+        "\n      OBS.: Caso λ não seja renderizado corretamente em seu terminal, este \n      char é um Lambda, indicando o autovalor da matriz A."
+    )
 
     for i in range(len(Lambda)):
         result = np.matmul(A, V[:, i])
@@ -401,16 +428,20 @@ def teste_2():
         )
 
         input("\n     Pressione [ENTER] para continuar.")
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[2K')
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[2K')
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
         print("      -")
 
     print(f"\n      Teste de ortogonalidade:\n")
-    print("      VVt =", np.array2string(np.matmul(V, np.transpose(V)), prefix = "            "))
+    print(
+        "      VVt =",
+        np.array2string(np.matmul(V, np.transpose(V)), prefix="            "),
+    )
 
     print("\n      Rotina de teste concluída! Obrigado pela execução!")
+
 
 def addBar(
     i: int,
@@ -436,14 +467,19 @@ def addBar(
         K[2 * j : 2 * j + 2, 2 * i : 2 * i + 2] += -local_stiffness
         K[2 * j : 2 * j + 2, 2 * j : 2 * j + 2] += local_stiffness
 
+
 def truss_from_file(filename):
-    with open(filename, encoding = 'utf-8') as file:
+    with open(filename, encoding="utf-8") as file:
         total_nodes, free_nodes, _ = map(int, file.readline().split())
         p, A, E = map(float, file.readline().split())
         E *= 1e9
 
-        treatline = lambda line: tuple( map(int, line.split()[:2]) ) + tuple( map(float, line.split()[2:]) )
-        bars = list(filter(lambda line: len(line) > 0, map(treatline, file.readlines())))
+        treatline = lambda line: tuple(map(int, line.split()[:2])) + tuple(
+            map(float, line.split()[2:])
+        )
+        bars = list(
+            filter(lambda line: len(line) > 0, map(treatline, file.readlines()))
+        )
 
         K = np.zeros((2 * free_nodes, 2 * free_nodes), float)
         M = np.zeros(free_nodes, float)
@@ -452,23 +488,25 @@ def truss_from_file(filename):
             (i, j, theta, L) = bar
             theta = np.deg2rad(theta)
             addBar(i - 1, j - 1, L, np.cos(theta), np.sin(theta), E, p, A, M, K)
-    
+
     return M, K, total_nodes, free_nodes, bars
 
+
 def teste_3():
-    print("""
+    print(
+        """
       >> Você selecionou a Aplicação em Treliças Planas.
       
-      - Realizando leitura de arquivo: 'input-c'\n""")
+      - Realizando leitura de arquivo: 'input-c'\n"""
+    )
 
-    M, K, total_nodes, free_nodes, bars = truss_from_file('input-c')
+    M, K, total_nodes, free_nodes, bars = truss_from_file("input-c")
 
     print("""\n      Matriz de Rigidez (K):\n""")
-    print("     K = ", np.array2string(K, prefix = "          "))
-
+    print("     K = ", np.array2string(K, prefix="          "))
 
     print("""\n      Matriz de Massa (M):\n""")
-    print("     M = ", np.array2string(np.diag(M), prefix = "          "))
+    print("     M = ", np.array2string(np.diag(M), prefix="          "))
 
     M = 1.0 / np.sqrt(M)
 
@@ -477,13 +515,19 @@ def teste_3():
             K[i, j] *= M[i // 2] * M[j // 2]
 
     print("""\n      Matriz da Equação Diferencial (K~):\n""")
-    print("      K~ = ", np.array2string(K, prefix = "            "))
+    print("      K~ = ", np.array2string(K, prefix="            "))
 
     alphas, betas, H = tridiagonalization(K)
     Lambda, _, V, _ = qr_algorithm(alphas, betas, H)
 
     print("""\n      K~ Tridiagonalizado:\n""")
-    print("      HK~Ht = ", np.array2string(np.diag(alphas) + np.diag(betas, -1) + np.diag(betas, 1), prefix = "               "))
+    print(
+        "      HK~Ht = ",
+        np.array2string(
+            np.diag(alphas) + np.diag(betas, -1) + np.diag(betas, 1),
+            prefix="               ",
+        ),
+    )
     print(f"\n      Autovalores Encontrados: {np.array(sorted(Lambda))}")
 
     frequencies = list(map(np.sqrt, sorted(Lambda)))
@@ -500,13 +544,20 @@ def teste_3():
     print(f"\n      5 menores Frequências Encontrados: {frequencies[:5]}")
     print("""\n      Modos de vibração associados às 5 menores frequências:\n""")
 
-    if str(input("\n      Deseja visualizar uma animação das treliças vibrando conforme cada modo de vibração? (S/n): ")).lower() != "n":
+    if (
+        str(
+            input(
+                "\n      Deseja visualizar uma animação das treliças vibrando conforme cada modo de vibração? (S/n): "
+            )
+        ).lower()
+        != "n"
+    ):
         titles = ["primeira", "segunda", "terceira", "quarta", "quinta"]
         scale = [100, 150, 100, 100, 150]
 
         for i in range(5):
             print(f"\n      Exibindo a {titles[i]} animação!")
-            
+
             freq = frequencies[i]
             mode = modes[i]
 
@@ -515,11 +566,15 @@ def teste_3():
 
             print(f"\n      Frequência natural de oscilação: {freq:.4f} rad/s")
             print(f"      Modo de vibração:")
-            print("      z = ", np.array2string(mode, prefix = "         "))
+            print("      z = ", np.array2string(mode, prefix="         "))
 
-            print(f"\n      Iremos excitar a treliça com uma condição inicial igual a {scale[i]} vezes o modo de oscilação associado a esta frequência.")
+            print(
+                f"\n      Iremos excitar a treliça com uma condição inicial igual a {scale[i]} vezes o modo de oscilação associado a esta frequência."
+            )
 
-            print(f"\n      Animação aberta. Por favor, feche a janela para prosseguir.")
+            print(
+                f"\n      Animação aberta. Por favor, feche a janela para prosseguir."
+            )
 
             fig = plt.figure()
             fig.set_size_inches(18.5, 10.5)
@@ -535,7 +590,9 @@ def teste_3():
             mat = ax.plot(X0, Y0, "o")
 
             for k, (i, j, _, _) in enumerate(bars):
-                bar_lines[k] = ax.plot([X0[i - 1], X0[j - 1]], [Y0[i - 1], Y0[j - 1]], c="k")
+                bar_lines[k] = ax.plot(
+                    [X0[i - 1], X0[j - 1]], [Y0[i - 1], Y0[j - 1]], c="k"
+                )
 
             patch = reduce(lambda a, b: a + b, bar_lines) + list(mat)
 
@@ -559,75 +616,96 @@ def teste_3():
             plt.show()
     print("\n      Rotina de teste concluída! Obrigado pela execução!")
 
+
 def teste_4():
-    print("""
+    print(
+        """
       >> Você selecionou o Teste com uma Matriz Arbitrária.
       
       Para este teste, há duas opções: 
         (1) Ler uma matriz de um arquivo
-        (2) Entrar com uma matriz, manualmente.\n""")
-    
+        (2) Entrar com uma matriz, manualmente.\n"""
+    )
+
     choice = int(input("      Escolha: "))
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[2K')
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[2K')
+    sys.stdout.write("\x1b[1A")
+    sys.stdout.write("\x1b[2K")
+    sys.stdout.write("\x1b[1A")
+    sys.stdout.write("\x1b[2K")
 
     if choice == 1:
-        print("""
+        print(
+            """
       Você escolheu o modo de leitura de uma matriz de um arquivo. Este arquivo DEVE
       ser formatado conforme as entradas para o teste A e B. Isto é, a primeira entrada
       do arquivo deve conter o tamanho da matriz e as linhas subsequentes contêm as
-      entradas, linha a linha. IMPORTANTE: a última linha não pode estar em branco.\n""")
+      entradas, linha a linha. IMPORTANTE: a última linha não pode estar em branco.\n"""
+        )
 
         filename = str(input("      Entre com o nome e extensão do arquivo: "))
         matrix = matrix_from_file(filename)
-    
+
     else:
-        n = int(input("""
-      Você escolheu o modo de entrada manual. Entre, primeiro, com o tamanho da matriz: """))
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[2K')
-        print(f"""
+        n = int(
+            input(
+                """
+      Você escolheu o modo de entrada manual. Entre, primeiro, com o tamanho da matriz: """
+            )
+        )
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
+        print(
+            f"""
       Você escolheu o modo de leitura de uma matriz de um arquivo. Agora, você deve inserir as {n}
       entradas de sua matriz. Para isto, digite a entrada e pressione ENTER, até que a matriz esteja
-      completa em sua exibição.\n""")
+      completa em sua exibição.\n"""
+        )
 
-        matrix = np.zeros((n,n))
+        matrix = np.zeros((n, n))
         for i in range(n):
             if i != 0:
-                print('')
+                print("")
             for j in range(n):
                 if i == 0:
-                    print("      A = [", end = "")
+                    print("      A = [", end="")
                 else:
-                    print("           ", end = "")
-                print("[", *matrix[i, :j], end = ' ')
-                matrix[i,j] = float((input("")))
-                sys.stdout.write('\x1b[1A')
+                    print("           ", end="")
+                print("[", *matrix[i, :j], end=" ")
+                matrix[i, j] = float((input("")))
+                sys.stdout.write("\x1b[1A")
             if i == 0:
-                print("      A = [", end = "")
+                print("      A = [", end="")
             else:
-                print("           ", end = "")
-            print("[", *matrix[i, :], "]", end = '')   
-        print("]")     
+                print("           ", end="")
+            print("[", *matrix[i, :], "]", end="")
+        print("]")
 
     if choice != 2:
         print("""      Matriz de entrada:\n""")
-        print("     ", np.array2string(matrix, prefix = "      "))
+        print("     ", np.array2string(matrix, prefix="      "))
 
     alphas, betas, H = tridiagonalization(matrix)
 
     print("""\n      Matriz tridiagonalizada:\n""")
-    print("     ", np.array2string(np.diag(betas, k = 1) + np.diag(betas, k = -1) + np.diag(alphas), prefix = "      "))
+    print(
+        "     ",
+        np.array2string(
+            np.diag(betas, k=1) + np.diag(betas, k=-1) + np.diag(alphas),
+            prefix="      ",
+        ),
+    )
 
     Lambda, _, V, _ = qr_algorithm(alphas, betas, H)
 
-    print(f"\n      Autovalores Encontrados:\t{np.array(sorted(Lambda, reverse = True))}")
+    print(
+        f"\n      Autovalores Encontrados:\t{np.array(sorted(Lambda, reverse = True))}"
+    )
     print(f"\n      Matriz de Autovetores:\n")
-    print("     ", np.array2string(V, prefix = "      "))
+    print("     ", np.array2string(V, prefix="      "))
 
-    print("\n      OBS.: Caso λ não seja renderizado corretamente em seu terminal, este \n      char é um Lambda, indicando o autovalor da matriz A.")
+    print(
+        "\n      OBS.: Caso λ não seja renderizado corretamente em seu terminal, este \n      char é um Lambda, indicando o autovalor da matriz A."
+    )
 
     for i in range(len(Lambda)):
         result = np.matmul(matrix, V[:, i])
@@ -645,14 +723,17 @@ def teste_4():
         )
 
         input("\n     Pressione [ENTER] para continuar.")
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[2K')
-        sys.stdout.write('\x1b[1A')
-        sys.stdout.write('\x1b[2K')
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
         print("      -")
 
     print(f"\n      Teste de ortogonalidade:\n")
-    print("      VVt =", np.array2string(np.matmul(V, np.transpose(V)), prefix = "            "))
+    print(
+        "      VVt =",
+        np.array2string(np.matmul(V, np.transpose(V)), prefix="            "),
+    )
 
     print("\n      Rotina de teste concluída! Obrigado pela execução!")
 
@@ -660,9 +741,13 @@ def teste_4():
 import sys
 
 if __name__ == "__main__":
-    np.set_printoptions(precision = 6, linewidth = 250, suppress = True, sign = ' ', threshold = 10, edgeitems = 5)
+    np.set_printoptions(
+        precision=6, linewidth=250, suppress=True, sign=" ", threshold=10, edgeitems=5
+    )
 
-    teste = int(input("""
+    teste = int(
+        input(
+            """
            _____ ____ ____       __  __    _    ____ _____ _ ____  _ 
           | ____|  _ \___ \     |  \/  |  / \  |  _ \___ // |___ \/ |
           |  _| | |_) |__) |____| |\/| | / _ \ | |_) ||_ \| | __) | |
@@ -684,7 +769,9 @@ if __name__ == "__main__":
       (3) Aplicação: Treliças Planas.
       (4) Matriz arbitrária.
 
-      Digite um número (1 - 4): """))
+      Digite um número (1 - 4): """
+        )
+    )
 
     if teste == 1:
         teste_1()
@@ -695,4 +782,4 @@ if __name__ == "__main__":
     elif teste == 4:
         teste_4()
     else:
-        print("\n      Inválido. Por favor, recomece, obedecendo as instruções.")    
+        print("\n      Inválido. Por favor, recomece, obedecendo as instruções.")
